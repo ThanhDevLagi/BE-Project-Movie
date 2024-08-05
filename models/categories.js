652d5831cafb5914a74d1f9dde2423ad58a7e3b6
Cao Hoàng Thành <160160995+ThanhDevLagi@users.noreply.github.com>
2024-08-05 15:43:48 +0700

    add action createAt categories

--- models/categories.js
+++ models/categories.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema;
 
 
 const categoriesSchema = new Schema({
-    name: { type: String, required: true }
+    name: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now }
 });
 
 
